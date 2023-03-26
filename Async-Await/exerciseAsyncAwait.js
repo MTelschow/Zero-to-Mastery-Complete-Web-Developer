306da3d21cbb5b9e04258cbfabca3b5d31555d07
Mathis Telschow <mathistelschow@gmail.com>
2023-03-26 22:45:29 +0200

    Solve exercise

--- Async-Await/exerciseAsyncAwait.js
+++ Async-Await/exerciseAsyncAwait.js
@@ -5,6 +5,12 @@ fetch("https://jsonplaceholder.typicode.com/users/")
   .then((response) => response.json())
   .then(console.log);
 
+
+async function fetchData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/");
+  const data = await response.json();
+  console.log(data);
+}
 // #2) ADVANCED: Update the function below from the video to also have
 // async await for this line: fetch(url).then(resp => resp.json())
 // So there shouldn't be any .then() calls anymore!
@@ -24,9 +30,41 @@ const getData = async function () {
   console.log("albums", albums);
 };
 
+const getDataAsync = async function () {
+  const [users, posts, albums] = await Promise.all(
+    urls.map(async (url) => {
+      const response = await fetch(url);
+      return await response.json();
+    })
+  );
+  console.log("users", users);
+  console.log("posta", posts);
+  console.log("albums", albums);
+};
+
+
+
+
 // #3)Add a try catch block to the #2 solution in order to catch any errors. // Now, use the given array containing an invalid url, so you console.log  //your error with 'oooooops'.
 const urls = [
   "https://jsonplaceholder.typicode.com/users",
   "https://jsonplaceholdeTYPO.typicode.com/posts",
   "https://jsonplaceholder.typicode.com/albums",
 ];
+
+const getDataAsync = async function () {
+  try {
+    const [users, posts, albums] = await Promise.all(
+      urls.map(async (url) => {
+        const response = await fetch(url);
+        return await response.json();
+      })
+    );
+    console.log("users", users);
+    console.log("posta", posts);
+    console.log("albums", albums);
+  } catch (error) {
+    console.log('oooooops', error);
+  }
+};
+getDataAsync();
